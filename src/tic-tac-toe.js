cab8596cec3f3af67f1b7874f20d87d1692bd144
Nastya Tsimanovich <nastya.cimanovich.97@gmail.com>
2019-10-18 09:42:11 +0300

    feat: add solution for 1 test

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,28 @@
 class TicTacToe {
     constructor() {
-
+        this.matrix=[[0,0,0],
+                     [0,0,0],
+                     [0,0,0]];
+        this.player='x';
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.player;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if(this.matrix[rowIndex][columnIndex]==0){
+            this.matrix[rowIndex][columnIndex]=this.player;
+            if(this.player=='x'){
+                this.player='o';
+            }
+            else{
+                this.player='x';
+            }
+        }
+        else{
+            this.player=this.player
+        }
     }
 
     isFinished() {
